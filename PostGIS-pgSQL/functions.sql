4c56df9a93d8f1b62287596da1a52d8f35865f29
brkypc <berkayyapici1998@gmail.com>
2021-05-30 00:00:20 +0300

    Update functions.sql

---
+++ PostGIS-pgSQL/functions.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION add_route(cId INT, tId INT)
+RETURNS void AS $$
+DECLARE
+line geometry;
+BEGIN
+CREATE TEMP TABLE temp_table AS SELECT geometry FROM users_locations WHERE trackingid = tId AND clientId=cId GROUP BY geometry, time ORDER BY time;
+SELECT ST_MakeLine(geometry) into line FROM temp_table;
+DROP TABLE temp_table;
+INSERT INTO users_routes(name,ref,type,class,z_order,clientId,trackingid,geometry) 
+VALUES('Rota',null,'motorway','motorways',39,cId,tId,line);
+END;
+$$ LANGUAGE 'plpgsql';
