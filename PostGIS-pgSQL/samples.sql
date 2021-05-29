255d46a3f9390230d43069767ffbb4b119d0e2ef
brkypc <berkayyapici1998@gmail.com>
2021-05-30 01:57:55 +0300

    Create samples.sql

---
+++ PostGIS-pgSQL/samples.sql
@@ -0,0 +1,5 @@
+INSERT INTO users_locations(latitude,longitude,time,clientId,trackingId) VALUES(41.015326,28.974420,1,1,1);
+INSERT INTO users_locations(latitude,longitude,time,clientId,trackingId) VALUES(41.015566,28.974380,2,1,1);
+INSERT INTO users_locations(latitude,longitude,time,clientId,trackingId) VALUES(41.015297,28.974790,3,1,1);
+INSERT INTO users_locations(latitude,longitude,time,clientId,trackingId) VALUES(41.015160,28.975030,4,1,1);
+INSERT INTO users_locations(latitude,longitude,time,clientId,trackingId) VALUES(41.015220,28.975080,5,1,1);
