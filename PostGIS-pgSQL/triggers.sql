2772d4eca2ac54c55e49aee77dbbd84b62131fe2
brkypc <berkayyapici1998@gmail.com>
2021-05-29 23:28:14 +0300

    Update triggers.sql

---
+++ PostGIS-pgSQL/triggers.sql
@@ -0,0 +1,20 @@
+-- users_locations table set geometry column trigger
+
+CREATE OR REPLACE FUNCTION make_geom_func()
+RETURNS trigger AS $$
+DECLARE
+geo geometry;
+lat NUMERIC;
+lon NUMERIC;
+BEGIN
+SELECT latitude INTO lat FROM users_locations WHERE osm_id = NEW.osm_id;
+SELECT longitude INTO lon FROM users_locations WHERE osm_id = NEW.osm_id;
+SELECT ST_SetSRID(ST_Point(lon,lat),3857) INTO geo;
+UPDATE users_locations SET geometry = geo WHERE osm_id = NEW.osm_id;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER geom_trigger AFTER INSERT ON users_locations
+FOR EACH ROW
+EXECUTE PROCEDURE make_geom_func();
