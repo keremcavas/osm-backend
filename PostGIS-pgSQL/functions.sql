037113aaa07005eb691cbddccda2d136dfadc030
brkypc <44416789+brkypc@users.noreply.github.com>
2021-06-02 17:35:46 +0300

    Update functions.sql

--- PostGIS-pgSQL/functions.sql
+++ PostGIS-pgSQL/functions.sql
@@ -5,7 +5,7 @@ line geometry;
 BEGIN
 CREATE TEMP TABLE temp_table
 AS SELECT geometry FROM users_locations WHERE trackingid=tId AND clientId=cId GROUP BY geometry, time ORDER BY time;
-SELECT ST_MakeLine(geometry) INTO line FROM temp_table;
+SELECT ST_Transform(ST_MakeLine(geometry),3857) INTO line FROM temp_table;
 DROP TABLE temp_table;
 INSERT INTO users_routes(name,ref,type,class,z_order,clientId,trackingid,geometry)
 VALUES('Rota',null,'motorway','motorways',39,cId,tId,line);
