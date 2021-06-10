fe5a7a2f1e8f16bec0dcd6a91429497f6f41ef6e
Kerem Çavaş <57960654+keremcavas@users.noreply.github.com>
2021-06-10 22:27:36 +0300

    Create closest routes finder to given point

--- PostGIS-pgSQL/functions.sql
+++ PostGIS-pgSQL/functions.sql
@@ -11,3 +11,32 @@ INSERT INTO users_routes(name,ref,type,class,z_order,clientId,trackingId,geometr
 VALUES('Rota',null,'motorway','motorways',39,cId,tId,line);
 END;
 $$ LANGUAGE 'plpgsql';
+
+-- find 5 closest routes to the given location point
+CREATE OR REPLACE FUNCTION get_routes_near_point(lat DOUBLE PRECISION, lon DOUBLE PRECISION)
+RETURNS TABLE (osm_id BIGINT,
+			   clientId INT,
+			   trackingId INT,
+			   time1 BIGINT
+) AS 
+$$
+DECLARE
+	route RECORD;
+BEGIN
+	FOR route IN (
+		SELECT 
+			r.osm_id,
+			r.trackingId,
+			r.clientId
+		FROM users_routes r
+		ORDER BY ST_Distance(ST_Transform(r.geometry, 4326), ST_SetSRID(ST_MakePoint(lon, lat), 4326))
+		LIMIT 5
+	) LOOP
+		osm_id := route.osm_id;
+		clientId := route.clientID;
+		trackingId := route.trackingID;
+		SELECT l.time INTO time1 FROM users_locations l WHERE route.trackingId = l.trackingId LIMIT 1;
+		RETURN NEXT;
+	END LOOP;
+END;
+$$ LANGUAGE 'plpgsql';
