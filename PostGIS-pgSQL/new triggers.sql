b1c8b697e6ec96c2f7ba050cc8937aa5c90abdb6
brkypc <44416789+brkypc@users.noreply.github.com>
2021-06-20 19:54:59 +0300

    Add files via upload

---
+++ PostGIS-pgSQL/new triggers.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION fill_color()
+RETURNS trigger AS $$
+DECLARE
+color text; color1 text; color2 text; color3 text;
+client_count int;
+BEGIN
+SELECT COUNT(osm_id) INTO client_count FROM users_routes WHERE clientid = NEW.clientid;
+IF client_count = 1 THEN
+SELECT floor(random() * 256)::text INTO color1;
+SELECT floor(random() * 256)::text INTO color2;
+SELECT floor(random() * 256)::text INTO color3;
+color := color1 || ' ' || color2 || ' ' || color3;
+ELSE
+SELECT route_color INTO color FROM users_routes WHERE clientid = NEW.clientid LIMIT 1;
+END IF;
+UPDATE users_routes SET route_color = color WHERE osm_id = NEW.osm_id;
+RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER color_trigger AFTER INSERT ON users_routes
+FOR EACH ROW
+EXECUTE PROCEDURE fill_color();
