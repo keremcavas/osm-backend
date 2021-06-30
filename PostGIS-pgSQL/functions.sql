34411a302ccf2a2a561de190eb17c0b12dec3a43
Kerem Çavaş <57960654+keremcavas@users.noreply.github.com>
2021-06-30 15:34:01 +0300

    Create queries that search on specific client

--- PostGIS-pgSQL/functions.sql
+++ PostGIS-pgSQL/functions.sql
@@ -66,7 +66,7 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
-
+-- find routes inside given rectangular area
 CREATE OR REPLACE FUNCTION get_routes_inside_area(
         lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION,
         lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION)
@@ -92,7 +92,7 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
-
+-- find routes inside given rectangular area and time interval
 CREATE OR REPLACE FUNCTION get_routes_inside_area_time_interval(
         lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION,
         lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION,
@@ -110,6 +110,119 @@ BEGIN
         SELECT r.trackingId, r.clientId
         FROM users_routes r
         WHERE ST_Intersects(ST_Transform(r.geometry, 4326), ST_MakeEnvelope(lon1, lat1, lon2, lat2, 4326))
+    ) LOOP 
+        clientId := route.clientID;
+        trackingId := route.trackingID;
+        SELECT l.time INTO time FROM users_locations l WHERE route.trackingId = l.trackingId LIMIT 1;
+        IF time >= startTime AND time <= endTime THEN
+            RETURN NEXT;
+        END IF;
+    END LOOP;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- find 5 closest routes to the given location point of specific user
+CREATE OR REPLACE FUNCTION get_routes_near_point_of_client(lat DOUBLE PRECISION, lon DOUBLE PRECISION, clnt INT)
+RETURNS TABLE (clientId INT,
+               trackingId INT,
+               "time" BIGINT
+) AS 
+$$
+DECLARE
+    route RECORD;
+BEGIN
+    FOR route IN (
+        SELECT 
+            r.trackingId,
+            r.clientId
+        FROM users_routes r
+        WHERE r.clientid = clnt
+        ORDER BY ST_Distance(ST_Transform(r.geometry, 4326), ST_SetSRID(ST_MakePoint(lon, lat), 4326))
+        LIMIT 5
+    ) LOOP
+        clientId := route.clientID;
+        trackingId := route.trackingID;
+        SELECT l.time INTO time FROM users_locations l WHERE route.trackingId = l.trackingId LIMIT 1;
+        RETURN NEXT;
+    END LOOP;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- find 5 closest routes to the given location point and time interval of specific user
+CREATE OR REPLACE FUNCTION get_routes_near_point_time_inteval_of_client(lat DOUBLE PRECISION, lon DOUBLE PRECISION, startTime BIGINT, endTime BIGINT, clnt INT)
+RETURNS TABLE (clientId INT,
+               trackingId INT,
+               "time" BIGINT
+) AS 
+$$
+DECLARE
+    route RECORD;
+BEGIN
+    FOR route IN (
+        SELECT 
+            r.trackingId,
+            r.clientId
+        FROM users_routes r
+        WHERE r.clientid = clnt
+        ORDER BY ST_Distance(ST_Transform(r.geometry, 4326), ST_SetSRID(ST_MakePoint(lon, lat), 4326))
+        LIMIT 5
+    ) LOOP
+        clientId := route.clientID;
+        trackingId := route.trackingID;
+        SELECT l.time INTO time FROM users_locations l WHERE route.trackingId = l.trackingId LIMIT 1;
+        IF time >= startTime AND time <= endTime THEN
+            RETURN NEXT;
+        END IF;
+    END LOOP;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- find routes of specific user inside given rectangular area
+CREATE OR REPLACE FUNCTION get_routes_inside_area_of_client(
+        lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION,
+        lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION,
+        clnt INT)
+RETURNS TABLE (
+        clientId INT,
+        trackingId INT,
+        "time" BIGINT
+) AS
+$$
+DECLARE
+    route RECORD;
+BEGIN
+    FOR route IN (
+        SELECT r.trackingId, r.clientId
+        FROM users_routes r
+        WHERE ST_Intersects(ST_Transform(r.geometry, 4326), ST_MakeEnvelope(lon1, lat1, lon2, lat2, 4326)) AND r.clientid = clnt
+    ) LOOP 
+        clientId := route.clientID;
+        trackingId := route.trackingID;
+        SELECT l.time INTO time FROM users_locations l WHERE route.trackingId = l.trackingId LIMIT 1;
+        RETURN NEXT;
+    END LOOP;
+END;
+$$ LANGUAGE 'plpgsql';
+
+-- find routes of specific user inside given rectangular area and time interval
+CREATE OR REPLACE FUNCTION get_routes_inside_area_time_interval_of_client(
+        lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION,
+        lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION,
+        startTime BIGINT, endTime BIGINT,
+        clnt INT)
+RETURNS TABLE (
+        clientId INT,
+        trackingId INT,
+        "time" BIGINT
+) AS
+$$
+DECLARE
+    route RECORD;
+BEGIN
+    FOR route IN (
+        SELECT r.trackingId, r.clientId
+        FROM users_routes r
+        WHERE ST_Intersects(ST_Transform(r.geometry, 4326), ST_MakeEnvelope(lon1, lat1, lon2, lat2, 4326)) AND r.clientid = clnt
     ) LOOP 
         clientId := route.clientID;
         trackingId := route.trackingID;
